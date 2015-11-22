b6f3216945dbbab64bac0014dbf0757d0585baba
MaciekLeks <maciej.leks@gmail.com>
2015-11-22 17:14:34 +0100

    Comment changed

--- tools.jl
+++ tools.jl
@@ -1,9 +1,9 @@
 answer{T}(answers::Vector{T},result::T) = begin
 	val,aindx = findmin(abs(answers - result))
-	result, convert(Char,aindx+96),answers[aindx] #add 96 t
+	result, convert(Char,aindx+96),answers[aindx] #adds 96 to transform number to letter
 end
 
 answer_min{T}(answers::Vector{T}) = begin
 	val,aindx = findmin(answers)
-	val, convert(Char,aindx+96) #add 96 t
+	val, convert(Char,aindx+96) #adds 96 to transform number to letter
 end
