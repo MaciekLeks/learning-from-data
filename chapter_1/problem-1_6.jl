d07b13e65d1dbb539760d842f95a0d050e80ec88
MaciekLeks <maciej.leks@gmail.com>
2016-03-09 14:10:30 +0100

    fixes

--- chapter_1/problem-1_6.jl
+++ chapter_1/problem-1_6.jl
@@ -1,3 +1,4 @@
+workspace()
 #Combinations
 C(k,n) = factorial(n)/(factorial(k)*factorial(n-k))
 
@@ -5,33 +6,34 @@ C(k,n) = factorial(n)/(factorial(k)*factorial(n-k))
 #Let X be a random variable of number of red marbles
 
 #P(X=k) =>
-binomial_probdist(; k=0, n=0, μ=0.0) = C(k,n)*μ^k*(1-μ)^n 
+binomial_probdist(; k=0, n=0, μ=0.0) = C(k,n)*μ^k*(1-μ)^(n-k) 
 
 #P(X<=k) =>
 binomial_distfun(; k=0, n=0, μ=0.0) = begin 
 	s = 0.0
 	for x=0:k
-		s += binomial_probdist(k=k,n=n,μ=μ)
+		s += binomial_probdist(k=x,n=n,μ=μ)
 	end
 	return s
 end
 
 #(a) We draw only one such sample
+const N = 10 #sample size
 println("problem 1.6\n(a): We draw only one such sample")
-@show binomial_probdist(k=0, n=10, μ=0.05)
-@show binomial_probdist(k=0, n=10, μ=0.5)
-@show binomial_probdist(k=0, n=10, μ=0.8) 
+@show binomial_probdist(k=0, n=N, μ=0.05)
+@show binomial_probdist(k=0, n=N, μ=0.5)
+@show binomial_probdist(k=0, n=N, μ=0.8) 
 
 #(b) We draw 1,000 independent samples
 println("(b): We draw 1,000 independent samples")
 samples = 1000
-@show (1 - binomial_distfun(k=10,n=10,μ=0.05) - binomial_distfun(k=0, n=10, μ=0.05)) ^ samples #(1 - probability of at least one marble in a sample)^samples
-@show (1 - binomial_distfun(k=10,n=10,μ=0.5) - binomial_distfun(k=0, n=10, μ=0.5)) ^ samples
-@show (1 - binomial_distfun(k=10,n=10,μ=0.8) - binomial_distfun(k=0, n=10, μ=0.8)) ^ samples
+@show 1 - (binomial_distfun(k=N, n=N, μ=0.05) - binomial_distfun(k=0, n=N, μ=0.05)) ^ samples #1 - (probability of at least one marble in a sample)^samples
+@show 1 - (binomial_distfun(k=N, n=N, μ=0.5) - binomial_distfun(k=0, n=N, μ=0.5)) ^ samples
+@show 1 - (binomial_distfun(k=N, n=N, μ=0.8) - binomial_distfun(k=0, n=N, μ=0.8)) ^ samples
 
 #(c) We draw 1,000,000 independent samples
 println("(c): We draw 1,000,000 independent samples")
 samples = 1000000
-@show (1 - binomial_distfun(k=10,n=10,μ=0.05) - binomial_distfun(k=0, n=10, μ=0.05)) ^ samples 
-@show (1 - binomial_distfun(k=10,n=10,μ=0.5) - binomial_distfun(k=0, n=10, μ=0.5)) ^ samples
-@show (1 - binomial_distfun(k=10,n=10,μ=0.8) - binomial_distfun(k=0, n=10, μ=0.8)) ^ samples
+@show 1 - (binomial_distfun(k=N, n=N, μ=0.05) - binomial_distfun(k=0, n=N, μ=0.05)) ^ samples 
+@show 1 - (binomial_distfun(k=N, n=N, μ=0.5) - binomial_distfun(k=0, n=N, μ=0.5)) ^ samples
+@show 1 - (binomial_distfun(k=N, n=N, μ=0.8) - binomial_distfun(k=0, n=N, μ=0.8)) ^ samples
